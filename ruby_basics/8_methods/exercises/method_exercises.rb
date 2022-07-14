7f11175dd92196671754b44148458066d6c1f08f
bruce <brucemoreno90@gmail.com>
2022-07-14 18:36:19 -0300

    More Ruby exercises

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -1,35 +1,57 @@
 # Since lesson #8 is on methods, you will be writing the entire method.
 # To gain more familiarity, look up the documentation for each hint.
 # Remember to unskip the corresponding tests one at a time.
-
 # method name: #ascii_translator
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
+def ascii_translator(number)
+    number.chr
+end
+
 
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(current_sports, favorite_sports)
+    current_sports.intersection(favorite_sports)
+end
+
 
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
+ def alphabetical_list(games)
+    games.uniq.sort
+ end
 
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(number = 7)
+    number = number
+     return "Today's lucky number is #{number}"
 
-
+    
+        
+end
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+def ascii_code(character)
+    if character.length == 0
+        return "Input Error"
+    else return character
+    end
+end
+
 
 
 # method name: #pet_pun
